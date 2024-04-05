6a3ea0a64ced715c10bd75b1bd970d170d608d0e
YourFriendYorik <kushnaryov04@inbox.ru>
2024-04-05 13:10:26 +0800

    Задание ПП2 1 и 2

---
+++ pp2-1/pp2-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+struct Buyer
+{
+    public int Number;
+    public string FullName;
+    public string Address;
+    public DateTime RegistrationDate;
+}
+class Program
+{
+    static void Main()
+    {
+        List<Buyer> buyers = new List<Buyer>();
+
+        // Добавим несколько покупателей для примера
+        buyers.Add(new Buyer { Number = 1, FullName = "Иванов Иван Иванович", Address = "ул. Пушкина, д.10", RegistrationDate = new DateTime(2022, 4, 15) });
+        buyers.Add(new Buyer { Number = 2, FullName = "Петров Петр Петрович", Address = "ул. Ленина, д.5", RegistrationDate = new DateTime(2022, 4, 3) });
+        buyers.Add(new Buyer { Number = 3, FullName = "Сидоров Сидор Сидорович", Address = "пр. Гагарина, д.3", RegistrationDate = new DateTime(2022, 4, 18) });
+        // Сортируем список покупателей по дате постановки на учет
+        buyers.Sort((x, y) => x.RegistrationDate.CompareTo(y.RegistrationDate));
+        // Выводим список покупателей в порядке очереди по датам постановки на учет
+        Console.WriteLine("Список покупателей в порядке очереди по датам постановки на учет:");
+        foreach (var buyer in buyers)
+        {
+            Console.WriteLine($"Покупатель {buyer.Number}: {buyer.FullName}, Адрес: {buyer.Address}, Дата постановки на учет: {buyer.RegistrationDate:dd.MM.yyyy}");
+        }
+    }
+}
---
+++ pp2/pp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string fileName = "output.txt"; // имя выходного файла
+        int N = 5; // целое число N
+
+        using (StreamWriter writer = File.CreateText(fileName))
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                string line = "";
+                for (char c = 'a'; c < 'a' + i; c++)
+                {
+                    line += c;
+                }
+                writer.WriteLine(line);
+            }
+        }
+
+        // Выводим сообщение об успешном завершении
+        Console.WriteLine($"Файл {fileName} успешно создан с содержимым по условию.");
+    }
+}
